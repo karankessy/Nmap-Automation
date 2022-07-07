00e5ab151e1faf6e7b7023e81cc892da1b67130d
0xErick <54462503+GrillingEvil@users.noreply.github.com>
2022-07-07 22:23:32 +0545

    Update autonmap.py

--- autonmap.py
+++ autonmap.py
@@ -7,42 +7,42 @@ scanner = nmap.PortScanner()
 print("Welcome, It's a simple nmap automation tool....")
 print("<<-------------------------------------------->>")
 
-ipadd = input 
-print ("IP you've entered is", ipadd)
-type(ipadd)
+ip_addr = input("Enter IP: ") 
+print ("IP you've entered is", ip_addr)
+type(ip_addr)
 
 
-resp = input  ("""
-               1) SYN ACK Scan
-               2) UDP Scan
-               3) Comprehensive Scan""")
+resp = input("""\n 1) SYN ACK Scan
+                   2) UDP Scan
+                   3) Comprehensive Scan\n""")
+
 print("You have selected option", resp)
 
 if resp == '1':
     print("Nmap Version: ", scanner.nmap_version)
-    scanner.scan(ipadd, '1-1024', '-v -sS')
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
     print(scanner.scaninfo)
-    print("IP Status: ", scanner[ipadd].state())
-    print(scanner[ipadd].all_protocols())
-    print("Open Ports: ", scanner[ipadd]['tcp'].keys())
+    print("IP Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
     
     
 elif resp == '2':
     print("Nmap Version: ", scanner.nmap_version)
-    scanner.scan(ipadd, '1-1024', '-v -sU')
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
     print(scanner.scaninfo)
-    print("IP Status: ", scanner[ipadd].state())
-    print(scanner[ipadd].all_protocols())
-    print("Open Ports: ", scanner[ipadd]['tcp'].keys())
+    print("IP Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
     
     
 elif resp == '3':
     print("Nmap Version: ", scanner.nmap_version)
-    scanner.scan(ipadd, '1-1024', '-v -sS -sV -sC -A -O')
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
     print(scanner.scaninfo)
-    print("IP Status: ", scanner[ipadd].state())
-    print(scanner[ipadd].all_protocols())
-    print("Open Ports: ", scanner[ipadd]['tcp'].keys())
+    print("IP Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
     
     
     
